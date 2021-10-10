47c5c55d3bd71e26aa6fca6f67064fea4f1897c3
Uber Marin <ubermarin0911@gmail.com>
2021-10-09 21:33:58 -0500

    Se implementa el patrón 'Unit of Work'

---
+++ Conteo y recaudo/Data/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using Conteo_y_recaudo.Entities;
+using Conteo_y_recaudo.Interfaces;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Conteo_y_recaudo.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RecaudoContext _context;
+        private Hashtable _repositories;
+
+        public UnitOfWork(RecaudoContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Complete()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity
+        {
+            if (_repositories == null) _repositories = new Hashtable();
+
+            var type = typeof(TEntity).Name;
+
+            if (!_repositories.ContainsKey(type))
+            {
+                var repositoryType = typeof(GenericRepository<>);
+                var repositoryInstance = Activator.CreateInstance(
+                    repositoryType.MakeGenericType(typeof(TEntity)), _context);
+
+                _repositories.Add(type, repositoryInstance);
+            }
+
+            return (IGenericRepository<TEntity>)_repositories[type];
+        }
+    }
+}
---
+++ Conteo y recaudo/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Conteo_y_recaudo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conteo_y_recaudo.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+
+        Task<int> Complete();
+    }
+}
